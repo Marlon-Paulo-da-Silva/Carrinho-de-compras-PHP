476bb3281dab541546a98f7491b5aaaa2d98cee0
Marlon Paulo <marlon.paulo.silva@outlook.com>
2022-12-14 16:52:39 -0300

    adicionando component e criando conexão BD

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
+
+  require_once('./php/component.php');
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,43 +20,12 @@
 
 <div class="container">
   <div class="row text-center py-5">
-    <div class="col-md-3 col-sm-6 my-3 my-md-0">
-      <form action="index.php" method="POST">
-        <div class="card shadow">
-          <div>
-            <img src="./upload/product1.png" alt="image1" class="img-fluid card-img-top">
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Produto 1</h5>
-            <h6>
-              <i class="fa fa-star"></i>
-              <i class="fa fa-star"></i>
-              <i class="fa fa-star"></i>
-              <i class="fa fa-star"></i>
-              <i class="far fa-star"></i>
-            </h6>
-            <p class="card-text">
-              Um rápido exemplo de texto para contruir o produto
-            </p>
-            <h5>
-              <small> <s class="text-secondary">R$ 620,00</s> </small>
-              <span class="price">R$ 560,00</span>
-            </h5>
-
-            <button type="submit" name="add" class="btn btn-warning my-3">Adicionar ao carrinho <i class="fa fa-shopping-cart"></i></button>
-          </div>
-        </div>
-      </form>
-    </div>
-    <div class="col-md-3 col-sm-6 my-3 my-md-0">
-
-    </div>
-    <div class="col-md-3 col-sm-6 my-3 my-md-0">
-
-    </div>
-    <div class="col-md-3 col-sm-6 my-3 my-md-0">
-
-    </div>
+    <?php
+      component('Produto 1','560,00','./upload/product1.png');
+      component('Produto 2','560,00','./upload/product2.png');
+      component('Produto 3','560,00','./upload/product3.png');
+      component('Produto 3','1000,00','./upload/product4.png');
+    ?>
   </div>
 </div>  
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
---
+++ php/component.php
@@ -0,0 +1,37 @@
+<?php
+
+function component($productName, $productPrice, $productImg){
+  $element = "
+
+    <div class=\"col-md-3 col-sm-6 my-3 my-md-0\">
+      <form action=\"index.php\" method=\"POST\">
+        <div class=\"card shadow\">
+          <div>
+            <img src=\"$productImg\" alt=\"image1\" class=\"img-fluid card-img-top\">
+          </div>
+          <div class=\"card-body\">
+            <h5 class=\"card-title\">".$productName."</h5>
+            <h6>
+              <i class=\"fa fa-star\"></i>
+              <i class=\"fa fa-star\"></i>
+              <i class=\"fa fa-star\"></i>
+              <i class=\"fa fa-star\"></i>
+              <i class=\"far fa-star\"></i>
+            </h6>
+            <p class=\"card-text\">
+              Um rápido exemplo de texto para contruir o produto
+            </p>
+            <h5>
+              <small> <s class=\"text-secondary\">R$ 620,00</s> </small>
+              <span class=\"price\">R$ $productPrice</span>
+            </h5>
+
+            <button type=\"submit\" name=\"add\" class=\"btn btn-warning my-3\">Adicionar ao carrinho <i class=\"fa fa-shopping-cart\"></i></button>
+          </div>
+        </div>
+      </form>
+    </div>
+  ";
+
+  echo $element;
+}
